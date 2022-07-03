5d9ca8942356afcc48a20fef029715a0d388c463
MelvinSam2000 <melvinsamxd@gmail.com>
2022-07-03 16:21:50 -0400

    Added desktop version with screen and joypad support

---
+++ nes-desktop/src/main.rs
@@ -0,0 +1,48 @@
+use std::cell::RefCell;
+use std::fs;
+use std::rc::Rc;
+
+use anyhow::Context;
+use anyhow::Result;
+use minifb::Key;
+use minifb::Window;
+use minifb::WindowOptions;
+
+use crate::nes::Nes;
+
+const WIDTH: usize = 240;
+const HEIGHT: usize = 256;
+
+fn main() -> Result<()> {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+    let window = Rc::new(RefCell::new(Window::new(
+        "Nes Emulator",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )?));
+
+    log::info!("Starting NES Emulator...");
+    let mut nes = Nes::new(window.clone());
+
+    let nes_rom_path = std::env::args().collect::<Vec<String>>();
+    let nes_rom_path = nes_rom_path.get(1)
+        .context("Cannot get file from CLI arguments")?;
+    let game_rom = fs::read(&nes_rom_path)?;
+    nes.load(&game_rom)?;
+
+    log::info!("Loaded game {:?}", &nes_rom_path);
+
+    while window.borrow().is_open() && !window.borrow().is_key_down(Key::Escape) {
+        nes.poll_key_press()?;
+        if let Err(err) = nes.clock() {
+            log::error!("Game crahed due to err: {}", err);
+            break;
+        }
+    }
+    Ok(())
+}
+
+pub mod nes;
+pub mod screen;
---
+++ nes-desktop/src/nes.rs
@@ -0,0 +1,86 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use nes::joypad::Button;
+use ::nes::nesaudio::NoAudio;
+use anyhow::Result;
+use minifb::{Window, Key};
+
+use crate::screen::NesScreen;
+
+pub struct Nes {
+    nes: ::nes::Nes,
+    window: Rc<RefCell<Window>>
+}
+
+impl Nes {
+    pub fn new(window: Rc<RefCell<Window>>) -> Self {
+        Self {
+            nes: ::nes::Nes::new(Box::new(NesScreen::new(window.clone())), Box::new(NoAudio)),
+            window,
+        }
+    }
+
+    pub fn clock(&mut self) -> Result<()> {
+        self.nes.clock()
+    }
+
+    pub fn step(&mut self) -> Result<()> {
+        let inst = self.nes.step()?;
+        log::info!("{inst}");
+        Ok(())
+    }
+
+    pub fn load(&mut self, rom_bytes: &[u8]) -> Result<()> {
+        self.nes.load(rom_bytes)?;
+        self.nes.reset()?;
+        Ok(())
+    }
+
+    pub fn poll_key_press(&mut self) -> Result<()>  {
+        let window = self.window.try_borrow();
+        if let Ok(window) = window {
+            if window.is_key_down(Key::Up) {
+                self.nes.press_btn(Button::UP, true)?;
+            } else {
+                self.nes.release_btn(Button::UP, true)?;
+            }
+            if window.is_key_down(Key::Down) {
+                self.nes.press_btn(Button::DOWN, true)?;
+            } else {
+                self.nes.release_btn(Button::DOWN, true)?;
+            }
+            if window.is_key_down(Key::Right) {
+                self.nes.press_btn(Button::RIGHT, true)?;
+            } else {
+                self.nes.release_btn(Button::RIGHT, true)?;
+            }
+            if window.is_key_down(Key::Left) {
+                self.nes.press_btn(Button::LEFT, true)?;
+            } else {
+                self.nes.release_btn(Button::LEFT, true)?;
+            }
+            if window.is_key_down(Key::A) {
+                self.nes.press_btn(Button::BTN_A, true)?;
+            } else {
+                self.nes.release_btn(Button::BTN_A, true)?;
+            }
+            if window.is_key_down(Key::S) {
+                self.nes.press_btn(Button::BTN_B, true)?;
+            } else {
+                self.nes.release_btn(Button::BTN_B, true)?;
+            }
+            if window.is_key_down(Key::Z) {
+                self.nes.press_btn(Button::START, true)?;
+            } else {
+                self.nes.release_btn(Button::START, true)?;
+            }
+            if window.is_key_down(Key::X) {
+                self.nes.press_btn(Button::SELECT, true)?;
+            } else {
+                self.nes.release_btn(Button::SELECT, true)?;
+            }
+        }
+        Ok(())
+    }
+}
